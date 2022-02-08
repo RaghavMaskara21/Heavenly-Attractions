ac97ba1731fe11bd07dd726d19bde34a97b6040f
RaghavMaskara21 <raghavmaskara@gmail.com>
2022-02-08 22:11:47 +0530

    created database

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const express= require('express');
-const app=express();
+const express = require('express');
 const path=require('path');
+const  mongoose  = require('mongoose');
+const Campground= require('./models/campground')
+const app=express();
+
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp',{
+        useNewUrlParser: true,
+        // useCreateIndex: true,
+        useUnifiedTopology: true
+});
+
+
+const db=mongoose.connection;
+db.on("error",console.error.bind(console, "connection error"));
+db.once("open",()=>{
+     console.log("Database connected");
+});
 
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname,'views'));
@@ -8,6 +24,11 @@ app.get('/',(req,res) =>{
     res.render('home');
 })
 
+app.get('/makecampground', async (req,res) =>{
+    const camp=new Campground({title:'my backyard'});
+    await camp.save();
+    res.send(camp); 
+})
 app.listen('3000', ()=>{
     console.log('Listening port 3000')
 })
