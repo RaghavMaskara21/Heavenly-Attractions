f9498fe3fb470655595f47de58dff92ba9b06a08
RaghavMaskara21 <raghavmaskara@gmail.com>
2022-02-10 00:07:50 +0530

    created random location and title seeds

--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,8 @@ const path=require('path');
 const  mongoose  = require('mongoose');
 const Campground= require('../models/campground');
 const app=express();
-
+const cities=require('./cities');
+const {places, descriptors} = require('./seedHelpers');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
         useNewUrlParser: true,
@@ -18,8 +19,19 @@ db.once("open",()=>{
      console.log("Database connected");
 });
 
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
 const seedDB = async () => {
     await Campground.deleteMany({});
-    const c= new Campground({title : 'purple field'});
-    await c.save();
+    for(let i=0;i<50;i++){
+        const random1000 = Math.floor(Math.random()*1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(places)}, ${sample(descriptors)}`
+        })
+        await camp.save();
+    }
 }
+seedDB().then(()=>{
+    mongoose.connection.close();
+})
