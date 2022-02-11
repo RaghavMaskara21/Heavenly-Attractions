4575d64dfc74bfe01f64d03c13b3b9ccd65f2099
RaghavMaskara21 <raghavmaskara@gmail.com>
2022-02-12 00:14:53 +0530

    added campgrounds show

--- app.js
+++ app.js
@@ -28,7 +28,10 @@ app.get('/campgrounds', async (req,res) =>{
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds});
 })
-
+app.get('/campgrounds/:id', async (req,res) =>{
+    const campground= await Campground.findById(req.params.id);
+    res.render('campgrounds/show',{campground});
+})
 
 app.listen('3000', ()=>{
     console.log('Listening port 3000')
