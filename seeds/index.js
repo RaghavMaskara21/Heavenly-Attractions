4a2303010c583922b4404151db18a79867883d03
RaghavMaskara21 <raghavmaskara@gmail.com>
2022-02-13 00:28:43 +0530

    added styles

--- seeds/index.js
+++ seeds/index.js
@@ -25,9 +25,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i=0;i<50;i++){
         const random1000 = Math.floor(Math.random()*1000);
+        const price=Math.floor(Math.random()*30)+10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(places)}, ${sample(descriptors)}`
+            title: `${sample(places)}, ${sample(descriptors)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid perspiciatis animi voluptatum sed! Itaque, ratione. Tempore laborum similique libero dignissimos unde eveniet soluta repellat magnam, facilis sint possimus ex.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem aliquid perspiciatis animi voluptatum sed! Itaque, ratione. Tempore laborum similique libero dignissimos unde eveniet soluta repellat magnam, facilis sint possimus ex.',
+            price
         })
         await camp.save();
     }
