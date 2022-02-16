3e98f36762f1fe2b573a23f2140aa4b1ac344e1d
RaghavMaskara21 <raghavmaskara@gmail.com>
2022-02-16 16:55:32 +0530

    creating reviews

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const ejsMate = require("ejs-mate");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError= require('./utils/ExpressErrors');
 const {CampgroundSchema} = require('./schemas');
+const Review=require('./models/reviews');
 const app = express();
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
@@ -101,6 +102,16 @@ app.delete(
     res.redirect(`/campgrounds`);
   })
 );
+
+app.post('/campgrounds/:id/reviews', catchAsync(async(req,res)=>{
+  const campground= await Campground.findById(req.params.id);
+  const review=new Review(req.body.review);
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+}));
+
 
 app.all('*',(req,res,next)=>{
     next(new ExpressError('Page not found',404));
