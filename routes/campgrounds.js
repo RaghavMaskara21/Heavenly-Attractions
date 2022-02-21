c04f7a76933bbd2d443fdb728827a635f790a1da
RaghavMaskara21 <raghavmaskara@gmail.com>
2022-02-21 23:09:01 +0530

    restructured routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -7,39 +7,32 @@ const Campground = require("../models/campground");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 const campgrounds = require("../controllers/campground");
 
-router.get("/", catchAsync(campgrounds.index));
+router
+  .route("/")
+  .get(catchAsync(campgrounds.index))
+  .post(
+    isLoggedIn,
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+  );
+  router.get("/new", isLoggedIn, campgrounds.renderNewForm);
+router
+  .route("/:id")
+  .get(catchAsync(campgrounds.showCampground))
+  .put(
+    isLoggedIn,
+    isAuthor,
+    validateCampground,
+    catchAsync(campgrounds.updateCampground)
+  )
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
-router.get("/new", isLoggedIn, campgrounds.renderNewForm);
-
-router.post(
-  "/",
-  isLoggedIn,
-  validateCampground,
-  catchAsync(campgrounds.createCampground)
-);
-
-router.get("/:id", catchAsync(campgrounds.showCampground));
 
 router.get(
   "/:id/edit",
   isLoggedIn,
   isAuthor,
   catchAsync(campgrounds.renderEditForm)
-);
-
-router.put(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  validateCampground,
-  catchAsync(campgrounds.updateCampground)
-);
-
-router.delete(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  catchAsync(campgrounds.deleteCampground)
 );
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,13 @@ const catchAsync=require('../utils/catchAsync');
 const passport=require('passport');
 const users= require('../controllers/users');
 
-router.get('/register',users.renderRegister);
-router.post('/register', catchAsync(users.register));
+router.route('/register')
+.get(users.renderRegister)
+.post( catchAsync(users.register));
 
-router.get('/login',users.renderLogin);
-router.post('/login', passport.authenticate('local',{failureFlash:true, failureRedirect:'/login'}), users.login)
+router.route('/login')
+.get(users.renderLogin)
+.post( passport.authenticate('local',{failureFlash:true, failureRedirect:'/login'}), users.login)
 
 router.get('/logout',users.logout)
 
