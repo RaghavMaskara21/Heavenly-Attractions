cf25bb2f399ae0cfa6981227081f420a3a6a76af
RaghavMaskara21 <raghavmaskara@gmail.com>
2022-02-24 00:08:57 +0530

    added sanitizing-html

--- schemas.js
+++ schemas.js
@@ -1,11 +1,42 @@
-const Joi=require('Joi');
+const BaseJoi=require('Joi');
+const sanitizeHtml=require('sanitize-html');
+
+const extension = (joi) => ({
+    type: "string",
+    base: joi.string(),
+    messages: {
+      "string.escapeHTML": "{{#label}} must not include HTML!",
+    },
+    rules: {
+      escapeHTML: {
+        validate(value, helpers) {
+          // escape symbols only (e.g. &, <)
+          const filtered = sanitizeHtml(value, {
+            allowedTags: false,
+            allowedAttributes: false,
+          });
+          // remove html
+          const clean = sanitizeHtml(filtered, {
+            allowedTags: [],
+            allowedAttributes: {},
+          });
+          // show error if html was present/removed
+          if (clean !== filtered) return helpers.error("string.escapeHTML");
+        },
+      },
+    },
+  });
+
+  const Joi=BaseJoi.extend(extension); 
+
+
 module.exports.CampgroundSchema= Joi.object({
     campground: Joi.object({
-        title: Joi.string().required(),
+        title: Joi.string().required().escapeHTML(),
         price: Joi.number().required().min(0),
         //image: Joi.string().required(),
-        location: Joi.string().required(),
-        description: Joi.string().required()
+        location: Joi.string().required().escapeHTML(),
+        description: Joi.string().required().escapeHTML()
     }).required(),
     deleteImages: Joi.array()
   });
@@ -13,6 +44,6 @@ module.exports.CampgroundSchema= Joi.object({
   module.exports.reviewSchema= Joi.object({
       review: Joi.object({
           rating: Joi.number().required(),
-          body: Joi.string().required() 
+          body: Joi.string().required().escapeHTML()
       }).required()
   });
