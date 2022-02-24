16d209d45c8d52b0d1b805641049898b395f23f8
RaghavMaskara21 <raghavmaskara@gmail.com>
2022-02-24 12:52:10 +0530

    fixed errors

--- app.js
+++ app.js
@@ -18,12 +18,71 @@ const passport=require('passport');
 const LocalStrategy= require('passport-local');
 const User=require('./models/user');
 const mongoSanitize= require('express-mongo-sanitize');
+//const helmet=require('helmet');
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
   // useCreateIndex: true,
   useUnifiedTopology: true,
 });
+
+
+// app.use(
+//   helmet({
+//     referrerPolicy: { policy: "no-referrer" },
+//     contentSecurityPolicy: false,
+//   })
+// );
+
+
+//app.use(helmet());
+
+
+// const scriptSrcUrls = [
+//   "https://stackpath.bootstrapcdn.com/",
+//   "https://api.tiles.mapbox.com/",
+//   "https://api.mapbox.com/",
+//   "https://kit.fontawesome.com/",
+//   "https://cdnjs.cloudflare.com/",
+//   "https://cdn.jsdelivr.net",
+// ];
+// //This is the array that needs added to
+// const styleSrcUrls = [
+//   "https://kit-free.fontawesome.com/",
+//   "https://api.mapbox.com/",
+//   "https://api.tiles.mapbox.com/",
+//   "https://fonts.googleapis.com/",
+//   "https://use.fontawesome.com/",
+//   "https://cdn.jsdelivr.net",
+// ];
+// const connectSrcUrls = [
+//   "https://api.mapbox.com/",
+//   "https://a.tiles.mapbox.com/",
+//   "https://b.tiles.mapbox.com/",
+//   "https://events.mapbox.com/",
+// ];
+// const fontSrcUrls = [];
+// app.use(
+//     helmet.contentSecurityPolicy({
+//         directives: {
+//             defaultSrc: [],
+//             connectSrc: ["'self'", ...connectSrcUrls],
+//             scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+//             styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+//             workerSrc: ["'self'", "blob:"],
+//             objectSrc: [],
+//             imgSrc: [
+//                 "'self'",
+//                 "blob:",
+//                 "data:",
+//                 "https://res.cloudinary.com/raghavmaskara", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+//                 "https://images.unsplash.com/",
+//             ],
+//             fontSrc: ["'self'", ...fontSrcUrls],
+//         },
+//     })
+// );
+
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error"));
@@ -41,12 +100,17 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(mongoSanitize());
 
+
+
+
 const sessionConfig= {
+  name:'session22',
   secret: 'useabettersecret',
   resave: false,
   saveUninitialized: true,
   cookie: {
     httpOnly:true,
+    //secure:true,
     expires: Date.now()+ 1000*60*60*24*7,
     maxAge:  1000*60*60*24*7
   }
